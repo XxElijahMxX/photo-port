ad950c33b43d09f3aabd06c174d01220d84eefad
Mugen <melton.sheridan@gmail.com>
2022-07-12 00:09:11 -0400

    added new test for the nav component

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -31,7 +31,6 @@ function ContactForm() {
         setErrorMessage('');
       }
     }
-    console.log('errorMessage', errorMessage);
   };
 
   return (
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,49 +1,37 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
-    currentCategory,
     contactSelected,
-    setContactSelected
+    currentCategory,
+    setContactSelected,
   } = props;
-
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
       <h2>
         <a data-testid="link" href="/">
-          <span role="img" aria-label="camera">
-            {" "}
-            📸
-          </span>{" "}
-          Oh Snap!
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a
-              data-testid="about"
-              href="#about"
-              onClick={() => setContactSelected(false)}
-            >
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
               About me
             </a>
           </li>
           <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span>Contact</span>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
               className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && `navActive`
-              }`}
+                currentCategory.name === category.name && !contactSelected && 'navActive'
+                }`}
               key={category.name}
             >
               <span
